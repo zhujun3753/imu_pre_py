5580155cf65043b47aef14048154024e574bd18b
zhujun <56210041+zhujun3753@users.noreply.github.com>
2025-09-03 21:15:17 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-# evo_traj euroc data/mav0/state_groundtruth_estimate0/data.csv --save_as_kitti
-# evo_traj euroc data/mav0/state_groundtruth_estimate0/data.csv --save_plot ape_tum_kitti.png
+# evo_traj euroc data/state_groundtruth_estimate0/data.csv --save_as_kitti
+# evo_traj euroc data/state_groundtruth_estimate0/data.csv --save_plot ape_tum_kitti.png
 
 # 进入容器后
-python main.py --euroc_root /app/data/MH_01_easy/mav0 --kf_hz 10 --scale_drop 0.3 --iters 8 --sigma_g 0.1
+python main.py --euroc_root /app/data --kf_hz 10 --scale_drop 0.3 --iters 8 --sigma_g 0.1
 
